d4ab718fa170d9a519037af64149329454c3ee5b
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2020-12-08 23:11:25 +0100

    day8

--- day8/day8.cpp
+++ day8/day8.cpp
@@ -28,55 +28,90 @@ list<string> get_lines()
     return lines;
 }
 
+class Change
+{
+public:
+    list<string>::iterator iter;
+    int line;
+    int accumulator;
+};
+
+Change do_instruction(string inst, int acc_change, list<string>::iterator iter, int line, int accumulator)
+{
+
+    if (inst._Equal("nop"))
+    {
+        line++;
+        ++iter;
+    }
+    else if (inst._Equal("acc"))
+    {
+        accumulator += acc_change;
+        line++;
+        ++iter;
+    }
+    else if (inst._Equal("jmp"))
+    {
+        int change = acc_change;
+        line += change;
+        while (change > 0)
+        {
+            ++iter;
+            change--;
+        }
+        while (change < 0)
+        {
+            --iter;
+            change++;
+        }
+    }
+    else
+    {
+        cout << "ERROR" << '\n';
+        exit(-1);
+    }
+
+    Change change;
+    change.accumulator = accumulator;
+    change.line = line;
+    change.iter = iter;
+    return change;
+}
+
+pair<int, int> get_lines_accumulator(list<string> &lines, list<string>::iterator iter, int accumulator, set<int> prev, int line, bool changed)
+{
+    if (prev.find(line) != prev.end() || line == lines.size())
+    {
+        return {line, accumulator};
+    }
+    prev.insert(line);
+    string inst = *iter;
+    string inst1 = inst.substr(0, inst.find(" "));
+    int inst2 = stoi(inst.substr(inst.find(" ") + 1));
+
+    if (!changed && (inst1._Equal("jmp") || inst1._Equal("nop")))
+    {
+        string alt_inst1 = inst1._Equal("jmp") ? "nop" : "jmp";
+
+        auto change = do_instruction(alt_inst1, inst2, iter, line, accumulator);
+        auto result = get_lines_accumulator(lines, change.iter, change.accumulator, prev, change.line, true);
+        if (result.first == lines.size())
+        {
+            return result;
+        }
+        
+    }
+
+    auto change = do_instruction(inst1, inst2, iter, line, accumulator);
+    return get_lines_accumulator(lines, change.iter, change.accumulator, prev, change.line, changed);
+}
+
 int main()
 {
     list<string> lines = get_lines();
-    int accumulator = 0;
-    auto iter = lines.begin();
     set<int> prev;
-    int line = 0;
-    while (prev.find(line) == prev.end())
-    {
-        cout << line << '\n';
-        prev.insert(line);
-        string inst = *iter;
-        string inst1 = inst.substr(0, inst.find(" "));
-        if (inst1._Equal("nop"))
-        {
-            line++;
-            ++iter;
-            continue;
-        }
-        else if (inst1._Equal("acc"))
-        {
-            accumulator += stoi(inst.substr(inst.find(" ") + 1));
-            line++;
-            ++iter;
-            continue;
-        }
-        else if (inst1._Equal("jmp"))
-        {
-            int change = stoi(inst.substr(inst.find(" ") + 1));
-            line += change;
-            while (change > 0)
-            {
-                ++iter;
-                change--;
-            }
-            while (change < 0)
-            {
-                --iter;
-                change++;
-            }
-        }
-        else
-        {
-            cout << "ERROR" << '\n';
-            exit(-1);
-        }
-    }
-
-    cout << "Day 1: " << accumulator << '\n';
+    cout << "Day 1: " << get_lines_accumulator(lines, lines.begin(), 0, prev, 0, true).second << '\n';
+    cout << "Day 2: " << get_lines_accumulator(lines, lines.begin(), 0, prev, 0, false).second << '\n';
 
     return 0;
 }
