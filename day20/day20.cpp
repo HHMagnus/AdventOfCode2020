4acb9fa0ad18c13aea00636d358f800a44baf95c
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-05-31 22:37:38 +0200

    correct positioning

--- day20/day20.cpp
+++ day20/day20.cpp
@@ -41,6 +41,10 @@ public:
         return normal.compare(other.normal) == 0 || normal.compare(other.rev) == 0;
     }
 
+    bool exact(Side &other) {
+        return normal.compare(other.normal) == 0;
+    }
+
     Side(string x) {
         normal = x;
         string revs = normal;
@@ -57,8 +61,6 @@ public:
     vector<Side> sides;
     int unique_sides;
 
-    Tile flip();
-    Tile rotate();
     bool contains_side(Side &other) {
         for (Side &side : sides) {
             if (side.equal(other)) {
@@ -66,6 +68,125 @@ public:
             }
         }
         return false;
+    }
+
+    bool contains_exact(Side &other) {
+        for (Side &side : sides) {
+            if (side.exact(other)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    Side& top() {
+        return sides.at(0);
+    }
+
+    Side& bottom() {
+        return sides.at(1);
+    }
+
+    Side& left() {
+        return sides.at(2);
+    }
+
+    Side& right() {
+        return sides.at(3);
+    }
+
+    Tile fix_top_left_corner() {
+        if (top().unique && left().unique) {
+            return copy();
+        }
+        if (top().unique && right().unique) {
+            return rotate();
+        }
+        if (right().unique && bottom().unique) {
+            return rotate().rotate();
+        }
+        if (bottom().unique && left().unique) {
+            return rotate().rotate().rotate();
+        }
+        cout << "Could not find side\n";
+        exit(-1);
+    }
+
+    Tile fix_to_right(Tile& right) {
+        Tile rr = rotate_to_right(right);
+        if (right.contains_exact(rr.left())) {
+            return rr;
+        }
+        return rr.flip_horizontal();
+    }
+
+    Tile rotate_to_right(Tile &right) {
+        if (right.contains_side(top())) {
+            return rotate();
+        }
+        if (right.contains_side(this->right())) {
+            return rotate().rotate();
+        }
+        if (right.contains_side(bottom())) {
+            return rotate().rotate().rotate();
+        }
+        return copy();
+    }
+
+    Tile fix_to_top(Tile& top) {
+        Tile tt = rotate_to_top(top);
+        if (top.contains_exact(tt.top())) {
+            return tt;
+        }
+        return tt.flip_vertical();
+    }
+
+    Tile rotate_to_top(Tile &top) {
+        if (top.contains_side(right())) {
+            return rotate();
+        }
+        if (top.contains_side(bottom())) {
+            return rotate().rotate();
+        }
+        if (top.contains_side(left())) {
+            return rotate().rotate().rotate();
+        }
+        return copy();
+    }
+
+    Tile rotate() {
+        int length = tiles.size();
+        vector<string> nn;
+        for (int x = 0; x < length; x++) {
+            string s;
+            for (int y = 0; y < length; y++) {
+                s += tiles.at(y).at(length-1-x);
+            }
+            nn.push_back(s);
+        }
+        return Tile(id, nn);
+    }
+
+    Tile flip_horizontal() {
+        vector<string> nt;
+        for (int i = tiles.size()-1; i >= 0; i--) {
+            nt.push_back(tiles.at(i));
+        }
+        return Tile(id, nt);
+    }
+
+    Tile flip_vertical() {
+        vector<string> nt;
+        for (string s : tiles) {
+            string c = s;
+            reverse(c.begin(), c.end());
+            nt.push_back(c);
+        }
+        return Tile(id, nt);
+    }
+
+    Tile copy() {
+        return Tile(id, tiles);
     }
 
     bool fits_border(Tile& other) {
@@ -262,6 +383,36 @@ int main()
         }
     }
 
+    vector<vector<Tile>> matched;
+
+    vector<Tile> first;
+    first.push_back(get_id(tiles, pos[0][0]).fix_top_left_corner());
+
+    for (int i = 1; i < length; i++) {
+        Tile cc = get_id(tiles, pos[0][i]);
+        first.push_back(cc.fix_to_right(first.at(i-1)));
+    }
+
+    matched.push_back(first);
+
+    for (int j = 1; j < length; j++) {
+        vector<Tile> next;
+        for (int i = 0; i < length; i++) {
+            Tile cc = get_id(tiles, pos[j][i]);
+            next.push_back(cc.fix_to_top(matched.at(j-1).at(i)));
+        }
+        matched.push_back(next);
+    }
+
+    for (vector<Tile>& p : matched) {
+        for (int i = 0; i < 10; i++) {
+            for (Tile& t : p) {
+                cout << t.tiles.at(i) << " ";
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
 
 
     return 0;
