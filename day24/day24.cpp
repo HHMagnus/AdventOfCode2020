a74711be0bab15bc632bdf3fae73cc671f57d2ab
Magnus Hovmann <8402802+HHMagnus@users.noreply.github.com>
2024-06-01 12:33:49 +0200

    day 24 part 2

--- day24/day24.cpp
+++ day24/day24.cpp
@@ -31,32 +31,56 @@ vector<string> get_lines()
     return lines;
 }
 
+pair<int, int> se(pair<int,int> curr) {
+    return make_pair(curr.first-1, curr.second+1);
+}
+
+pair<int, int> sw(pair<int,int> curr) {
+    return make_pair(curr.first, curr.second+1);
+}
+
+pair<int, int> ne(pair<int,int> curr) {
+    return make_pair(curr.first, curr.second-1);
+}
+
+pair<int, int> nw(pair<int,int> curr) {
+    return make_pair(curr.first+1, curr.second-1);
+}
+
+pair<int, int> e(pair<int,int> curr) {
+    return make_pair(curr.first-1, curr.second);
+}
+
+pair<int, int> w(pair<int,int> curr) {
+    return make_pair(curr.first+1, curr.second);
+}
+
 pair<int, int> coord(string line) {
     pair<int, int> curr = make_pair(0, 0);
     while (line.size() > 0) {
         if (line.rfind("se", 0) == 0) {
             line = line.substr(2, line.size()-2);
-            curr = make_pair(curr.first-1, curr.second+1);
+            curr = se(curr);
         }
         if (line.rfind("sw", 0) == 0) {
             line = line.substr(2, line.size()-2);
-            curr = make_pair(curr.first, curr.second+1);
+            curr = sw(curr);
         }
         if (line.rfind("ne", 0) == 0) {
             line = line.substr(2, line.size()-2);
-            curr = make_pair(curr.first, curr.second-1);
+            curr = ne(curr);
         }
         if (line.rfind("nw", 0) == 0) {
             line = line.substr(2, line.size()-2);
-            curr = make_pair(curr.first+1, curr.second-1);
+            curr = nw(curr);
         }
         if (line.rfind("e", 0) == 0) {
-            curr = make_pair(curr.first-1, curr.second);
             line = line.substr(1, line.size()-1);
+            curr = e(curr);
         }
         if (line.rfind("w", 0) == 0) {
             line = line.substr(1, line.size()-1);
-            curr = make_pair(curr.first+1, curr.second);
+            curr = w(curr);
         }
     }
     return curr;
@@ -79,6 +103,51 @@ int main()
     }
 
     cout << "Day 24 part 1: " << blacks.size() << "\n";
+
+    for (int day = 0; day < 100; day++) {
+        set<pair<int, int>> new_blacks;
+
+        set<pair<int, int>> considered;
+        for (auto x : blacks) {
+            considered.insert(x);
+            considered.insert(e(x));
+            considered.insert(w(x));
+            considered.insert(se(x));
+            considered.insert(sw(x));
+            considered.insert(ne(x));
+            considered.insert(nw(x));
+        }
+
+        for (auto x : considered) {
+            set<pair<int, int>> neigh;
+            neigh.insert(e(x));
+            neigh.insert(w(x));
+            neigh.insert(se(x));
+            neigh.insert(sw(x));
+            neigh.insert(ne(x));
+            neigh.insert(nw(x));
+
+            int adjacent = 0;
+            for (auto a : neigh) {
+                if (blacks.find(a) != blacks.end())
+                    adjacent++;
+            }
+
+            bool is_black = blacks.find(x) != blacks.end();
+
+            if (is_black && adjacent != 0 && adjacent < 3) {
+                new_blacks.insert(x);
+            }
+
+            if (!is_black && adjacent == 2) {
+                new_blacks.insert(x);
+            }
+        }
+
+        blacks = new_blacks;
+    }
+
+    cout << "Day 24 part 2: " << blacks.size() << "\n";
 
     return 0;
 }
